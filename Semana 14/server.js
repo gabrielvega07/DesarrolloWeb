366fa8d52f962d97af3d051534c2d4859d6f530f
gabrielvega07 <79347680+gabrielvega07@users.noreply.github.com>
2023-10-25 00:46:51 -0600

    Add files via upload

---
+++ Semana 14/server.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const bodyParser = require('./api');
+
+const api = require('./api');
+
+const port = 3000;
+const app = express();
+
+app.listen(port, function () {
+    console.log('escuchando peticiones en el port ' + port);
+});
+
+// Analiza el texto como datos codificados en la URL para usarlos
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+//define la ruta
+app.use('/api', api);
+
